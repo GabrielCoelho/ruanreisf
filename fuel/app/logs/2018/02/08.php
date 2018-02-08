c7c2adb5bc8cbdb6d99de183b405dc2b7955fc2d
Gabriel Coelho Soares <soares@workmail.com>
2018-02-08 15:27:57 -0200

    Description: Files that have been created or modified for the command "oil server" works and the Developer could see the website

---
+++ fuel/app/logs/2018/02/08.php
@@ -0,0 +1,58 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2018-02-08 15:01:34 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-08 15:01:46 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-08 15:01:49 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-08 15:01:51 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2018-02-08 15:01:51 --> Compile Error - Cannot declare class CPanel, because the name is already in use in /home/gaabrielcooelho/Documentos/WebSites/ruanreisf/fuel/app/classes/controller/cpanel.php on line 0
+WARNING - 2018-02-08 15:02:22 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2018-02-08 15:02:22 --> Fatal Error - Class 'Controller\CPanel\Controller_Template' not found in /home/gaabrielcooelho/Documentos/WebSites/ruanreisf/fuel/app/classes/controller/cpanel.php on line 5
+WARNING - 2018-02-08 15:03:29 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2018-02-08 15:03:29 --> Error - The requested view could not be found: template.php in /home/gaabrielcooelho/Documentos/WebSites/ruanreisf/fuel/core/classes/view.php on line 440
+WARNING - 2018-02-08 15:03:30 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2018-02-08 15:03:30 --> Error - The requested view could not be found: template.php in /home/gaabrielcooelho/Documentos/WebSites/ruanreisf/fuel/core/classes/view.php on line 440
+WARNING - 2018-02-08 15:03:52 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2018-02-08 15:03:52 --> Error - The requested view could not be found: template.php in /home/gaabrielcooelho/Documentos/WebSites/ruanreisf/fuel/core/classes/view.php on line 440
+WARNING - 2018-02-08 15:03:53 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2018-02-08 15:03:53 --> Error - The requested view could not be found: template.php in /home/gaabrielcooelho/Documentos/WebSites/ruanreisf/fuel/core/classes/view.php on line 440
+WARNING - 2018-02-08 15:04:55 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2018-02-08 15:04:55 --> Error - Could not find asset: app.css in /home/gaabrielcooelho/Documentos/WebSites/ruanreisf/fuel/core/classes/asset/instance.php on line 413
+WARNING - 2018-02-08 15:05:16 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-08 15:06:03 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-08 15:06:04 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-08 15:06:06 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-08 15:07:59 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-08 15:08:23 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-08 15:08:25 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-08 15:08:25 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-08 15:08:44 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-08 15:09:32 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-08 15:09:34 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-08 15:09:35 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-08 15:09:46 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-08 15:09:47 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-08 15:09:47 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-08 15:09:48 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-08 15:09:48 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-08 15:10:01 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-08 15:10:02 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-08 15:10:22 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-08 15:11:00 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-08 15:11:24 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-08 15:11:51 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-08 15:12:26 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-08 15:13:16 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-08 15:13:57 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-08 15:14:42 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-08 15:15:15 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-08 15:15:56 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-08 15:17:12 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-08 15:17:51 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-08 15:18:06 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-08 15:18:15 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-08 15:18:24 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-08 15:18:48 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-08 15:18:56 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-08 15:19:15 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-08 15:19:21 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-08 15:22:20 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
