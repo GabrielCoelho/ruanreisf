511e750c8c7c18a109df8194104ae527094ca21d
Gabriel Coelho Soares <soares@workmail.com>
2018-02-15 10:34:09 -0200

    Log file - this commit is'nt a new pattern

---
+++ fuel/app/logs/2018/02/15.php
@@ -0,0 +1,5 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2018-02-15 10:26:24 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-15 10:26:37 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2018-02-15 10:26:41 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
