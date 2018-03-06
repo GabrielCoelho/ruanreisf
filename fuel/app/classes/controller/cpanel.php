68324f72230978c66073a8c9a3def38d9ef029dc
Gabriel Coelho Soares <soares@workmail.com>
2018-03-06 19:55:42 -0300

    Modified: Working on add and delete the categories items Description: Working fine by now, but there's some addings to do.

--- fuel/app/classes/controller/cpanel.php
+++ fuel/app/classes/controller/cpanel.php
@@ -17,4 +17,26 @@ class Controller_CPanel extends Controller_Template
         $data = array('categoria' => $all_category);
         $this->template->content = View::forge('superuser/cpanel-category', $data);
     }
+
+    public function action_add_category()
+    {
+        $new_category = $_POST['category'];
+        $props = array('nome_categoria' => $new_category);
+
+        $new = Model_Categoria::forge($props);
+        $new->save();
+
+        Response::redirect('controle/categoria');
+    }
+
+    public function action_delete_category()
+    {
+        $which_delete   =   $_POST['buttonClicked'];
+        $get_idby_explode =   explode('|', $which_delete);
+        $id_delete  =   $get_idby_explode[1];  
+        $entry  =   Model_Categoria::find($id_delete);
+        $entry->delete();
+
+        Response::redirect('controle/categoria');
+    }
 }
--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -1,8 +1,13 @@
 <?php
 return array(
-	'_root_'  => 'welcome/index',  // The default route
-	'_404_'   => 'welcome/404',    // The main 404 route
-	'controle' => 'cpanel/cpanel_index',
-	'controle/categoria' => 'cpanel/cpanel_category',
+	'_root_'				=>	'welcome/index',  // The default route
+	'_404_'					=>	'welcome/404',    // The main 404 route
+	'controle'				=>	'cpanel/cpanel_index',
+	'controle/categoria'	=>	'cpanel/cpanel_category',
+
+
+
+	'addCategory'			=>	'cpanel/add_category',
+	'deleteCategory'		=>	'cpanel/delete_category', 
 	'hello(/:name)?' => array('welcome/hello', 'name' => 'hello'),
 );
