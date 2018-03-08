7962d3f6f4fa39118cbde035f1942199fd0289fa
Gabriel Coelho Soares <soares@workmail.com>
2018-03-08 10:05:45 -0300

    Added: Trigger after delete Description: After deleting a category, the id keeps the AutoIncrement where it ends

--- fuel/app/classes/controller/cpanel.php
+++ fuel/app/classes/controller/cpanel.php
@@ -11,32 +11,50 @@ class Controller_CPanel extends Controller_Template
         $this->template->content = View::forge('superuser/cpanel-index', $data);
     }
 
-    public function action_cpanel_category()
-    {
-        $all_category = Model_Categoria::find('all');
-        $data = array('categoria' => $all_category);
-        $this->template->content = View::forge('superuser/cpanel-category', $data);
-    }
+    /**
+     * Category 
+     */
+        public function action_cpanel_category()
+        {
+            $all_category = Model_Categoria::find('all');
+            $data = array('categoria' => $all_category);
+            $this->template->content = View::forge('superuser/cpanel-category', $data);
+        }
 
-    public function action_add_category()
-    {
-        $new_category = $_POST['category'];
-        $props = array('nome_categoria' => $new_category);
+        public function action_add_category()
+        {
+            $new_category = $_POST['category'];
+            $props = array('nome_categoria' => $new_category);
 
-        $new = Model_Categoria::forge($props);
-        $new->save();
+            $new = Model_Categoria::forge($props);
+            $new->save();
 
-        Response::redirect('controle/categoria');
-    }
+            Response::redirect('controle/categoria');
+        }
 
-    public function action_delete_category()
-    {
-        $which_delete   =   $_POST['buttonClicked'];
-        $get_idby_explode =   explode('|', $which_delete);
-        $id_delete  =   $get_idby_explode[1];  
-        $entry  =   Model_Categoria::find($id_delete);
-        $entry->delete();
+        public function action_delete_category()
+        {
+            //Delete Section
+            $which_delete   =   $_POST['buttonClicked'];
+            $get_idby_explode =   explode('|', $which_delete);
+            $id_delete  =   $get_idby_explode[1];  
+            $entry  =   Model_Categoria::find($id_delete);
+            $entry->delete();
 
-        Response::redirect('controle/categoria');
-    }
+            //Trigger to keep the AutoIncrement
+            $last_entry = Model_Categoria::find('last');
+            $number_autoinc = $last_entry['id_categoria'];
+            $number_autoinc += 1;
+            $query = DB::query('ALTER TABLE categoria AUTO_INCREMENT='.$number_autoinc.';');
+            $query->execute();
+            Response::redirect('controle/categoria');
+        }
+    
+    /**
+     * História
+     */
+        public function action_historia()
+        {
+            
+        }
 }
