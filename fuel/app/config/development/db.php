ccb718e53980aa6163cb6ad27f96da22628fac83
Gabriel Coelho <soares@workmail.com>
2018-03-12 23:28:40 -0300

    Modified: development database login Description: Did it after restoring the SO and database

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -8,7 +8,7 @@ return array(
 		'connection'  => array(
 			'dsn'        => 'mysql:host=127.0.0.1:3306;dbname=ruanreis-dev',
 			'username'   => 'gabriel',
-			'password'   => 'admin123',
+			'password'   => 'gabrielTI13',
 		),
 	),
 );
