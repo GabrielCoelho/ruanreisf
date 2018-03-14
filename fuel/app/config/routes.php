216be29c75ab743475d9f1ff3e3fd570562ff5e3
Gabriel Coelho <soares@workmail.com>
2018-03-14 15:16:33 -0300

    Modified: Added some comments in file Description: keep it easy to understand

--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -2,13 +2,20 @@
 return array(
 	'_root_'				=>	'welcome/index',  // The default route
 	'_404_'					=>	'welcome/404',    // The main 404 route
-	'controle'				=>	'cpanel/cpanel_index',
-	'controle/categoria'	=>	'cpanel/cpanel_category',
-	'controle/historias'	=>	'cpanel/cpanel_historia',
+
+	/**
+	 * Control Panel Views
+	 */
+		'controle'				=>	'cpanel/cpanel_index',
+		'controle/categoria'	=>	'cpanel/cpanel_category',
+		'controle/historias'	=>	'cpanel/cpanel_historia',
+		'controle/addHistoria'	=>	'cpanel/add_historia',
 
 
-	'addHistoria'			=>	'cpanel/add_historia',
-	'addCategory'			=>	'cpanel/add_category',
-	'deleteCategory'		=>	'cpanel/delete_category', 
-	'hello(/:name)?' => array('welcome/hello', 'name' => 'hello'),
+		/**
+		 * Control Panel intern functions
+		 */
+			'addCategory'			=>	'cpanel/add_category',
+			'deleteCategory'		=>	'cpanel/delete_category', 
+			'hello(/:name)?' => array('welcome/hello', 'name' => 'hello'),
 );
