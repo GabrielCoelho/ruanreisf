a66f81bdb2497b3418a961d1bf7e3907c58878c4
Gabriel Coelho <soares@workmail.com>
2018-03-14 15:11:57 -0300

    Added: Page content Description: added the form to create new stories

---
+++ fuel/app/views/superuser/add-history.php
@@ -0,0 +1,18 @@
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12 col-md-8 offset-md-2">
+            <form action="">
+                <div class="form-row">
+                    <div class="form-group col-sm-12 col-md-6">
+                        <label for="storyName">Nome do Ensaio:</label>
+                        <input type="text" name="nomeEnsaio" id="storyName" placeholder="ARRAY">
+                    </div>
+                    <div class="form-group col-sm-12 col-md-6">
+                        <label for="storyDate">Data do Ensaio</label>
+                        <input type="date" name="dataEnsaio" id="storyDate">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
