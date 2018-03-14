dab63757939f39786faf9b72347c7cd957002ace
Gabriel Coelho <soares@workmail.com>
2018-03-14 10:19:53 -0300

    Added: Created the view of the new route Description: Working in this part right now - list view or table view? wondering...

---
+++ fuel/app/views/superuser/cpanel-history.php
@@ -0,0 +1,11 @@
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12">
+            <!-- List show with PHP -->
+            <?php foreach ($historia as $ensaio) { ?>        
+                
+            <?php } ?>
+            <!-- Closing list show -->
+        </div> <!-- Col-sm-12 closing -->
+    </div> <!-- Row closing -->
+</div> <!-- Container closing -->
