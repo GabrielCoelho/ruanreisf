533a04fbb03535bc2316c29c39330d5ac9828d70
Gabriel Coelho <soares@workmail.com>
2018-05-25 00:56:00 -0300

    Removendo todos os arquivos antigos do repositório do GitHub.

--- fuel/app/logs/2018/01/30.php
+++
@@ -1,4 +0,0 @@
-<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
-
-WARNING - 2018-01-30 10:39:37 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
-WARNING - 2018-01-30 10:39:45 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
