da194edfe5fc9d6c9cdae6d090ff0591a4264bcd
Gabriel Coelho <soares@workmail.com>
2018-06-12 00:34:39 -0300

    Adição da barra com as informações sociais do cliente

--- pagina-contato.php
+++ pagina-contato.php
@@ -5,9 +5,67 @@ get_header();
 
 <div class="bg-contato-box">
     <div class="container">
-        <span class="texto-contato">
+        <span class="texto-contato animated fadeInRight">
             Sua história merece ser eternizada!
         </span>
+    </div>
+</div>
+<div class="barra-social">
+    <div class="container">
+        <div class="row">
+            <!-- Facebook -->
+            <div class="col-md-3">
+                <a href="http://facebook.com/RuanR.Fotografia/" target="_blank">
+                    <div class="float-left mx-2">
+                        <i class="ion-social-facebook"></i>
+                    </div> 
+                    <div class="float-right">
+                        Facebook
+                        <br> /RuanR.Fotografia/
+                    </div>
+                </a>
+            </div>
+            <!-- Fim Facebook -->
+            <!-- WhatsApp -->
+            <div class="col-md-3">
+                <a href="tel:19998050851">
+                    <div class="float-left mx-2">
+                        <i class="ion-social-whatsapp"></i>
+                    </div> 
+                    <div class="float-right">
+                        Whatsapp
+                        <br> 19 99805 0851
+                    </div>
+                </a>
+            </div>
+            <!-- Fim Whatsapp -->
+            <!-- Instagram -->
+            <div class="col-md-3">
+                 <a href="http://instagram.com/ruan_reis/" target="_blank">
+                    <div class="float-left mx-2">
+                        <i class="ion-social-instagram"></i>
+                    </div> 
+                    <div class="float-right">
+                        Instagram
+                        <br> @ruan_reis
+                    </div>
+                </a>
+            </div>
+            <!-- Fim Instagram -->
+            <!-- Email -->
+            <div class="col-md-3">
+                <a href="mailto:ruanjreis@gmail.com">
+                    <div class="float-left mx-2">
+                        <i class="ion-email"></i>
+                    </div> 
+                    <div class="float-right">
+                        Email
+                        <br> ruanjreis@gmail.com
+                    </div>
+                </a>
+            </div>
+            <!-- Fim email -->
+        </div>
     </div>
 </div>
 
