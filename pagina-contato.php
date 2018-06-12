a93c1f386fffed7967f276413f1dfecdd6acaf11
Gabriel Coelho <soares@workmail.com>
2018-06-11 23:59:12 -0300

    Criação da página de contato. Primeiras estilizações da página!

---
+++ pagina-contato.php
@@ -0,0 +1,14 @@
+<?php
+// Template Name: Contato
+get_header();
+?>
+
+<div class="bg-contato-box">
+    <div class="container">
+        <span class="texto-contato">
+            Sua história merece ser eternizada!
+        </span>
+    </div>
+</div>
+
+<?php get_footer();?>
