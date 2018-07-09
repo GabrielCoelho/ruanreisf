ac93606623ca25488c802cc74a391f40456a0806
Gabriel Coelho <soares@workmail.com>
2018-07-09 18:13:27 -0300

    Dando alguns toques visuais, ainda não está responsivo

--- pagina-parceiro.php
+++ pagina-parceiro.php
@@ -3,8 +3,10 @@
 get_header();?>
 
 <div class="bg-parceiro-box">
-    <div class="centered">
-        <img src="<?= get_template_directory_uri();?>/uploads/graphis.jpg" alt="Grafis" class="img-fluid">
+    <div class="mascara">
+        <div class="centered">
+            <img src="<?= get_template_directory_uri();?>/uploads/graphis.jpg" alt="Grafis" class="img-fluid">
+        </div>
     </div>
 </div>
 
