75d1169c71ecaa386f68a5044d3d6b624ec42b8d
Gabriel Coelho <soares@workmail.com>
2018-07-09 12:26:30 -0300

    Começando a página de Parceiros Já adicionado os arquivos do logo e do background da graphis

---
+++ pagina-parceiro.php
@@ -0,0 +1,11 @@
+<?php
+// Template Name: Parceiros
+get_header();?>
+
+<div class="bg-parceiro-box">
+    <div class="centered">
+        <img src="<?= get_template_directory_uri();?>/uploads/graphis.jpg" alt="Grafis" class="img-fluid">
+    </div>
+</div>
+
+<?php get_footer();?>
