44318157ed5c5eafccbb2dfcf72ebe10f9896831
Gabriel Coelho <soares@workmail.com>
2018-07-20 15:12:32 -0300

    Página de todos os posts completa.

--- archive.php
+++ archive.php
@@ -2,21 +2,50 @@
 // Template Name: OqFazemos
 get_header(); ?>
 
-<div class="container">
-    <div class="row justify-content-center">
-        <ul class="d-inline-flex text-center" style="list-style-type: none;">
-            <li class="pr-4"> Ensaios </li>
-            <li class="pr-4"> Casamentos </li>
-            <li class="pr-4"> Aniversários </li>
-            <li class="pr-4"> Família </li>
-            <li class="pr-4"> Todos </li>
-        </ul>
+<div class="bg_black">
+    <div class="container">
+        <div class="row justify-content-center">
+            <ul class="d-inline-flex text-center menu-arquivo">
+                <li class="pr-4"> Ensaios </li>
+                <li class="pr-4"> Casamentos </li>
+                <li class="pr-4"> Aniversários </li>
+                <li class="pr-4"> Família </li>
+                <li class="pr-4"> Todos </li>
+            </ul>
+        </div>
     </div>
 </div>
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<div class="background-posts">
+    <div class="container py-3">
+        <div class="row justify-content-center">
+            <h2 class="titulo-arquivo">Todos</h2>
+        </div>
+    </div>
 
-<?php endwhile; else : ?>
-	<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
-<?php endif; ?>
+    <?php 
+    // query para pegar os posts
+        $querySearch = new WP_Query('all');
+    ?>
+
+    <div class="container mt-2">
+        <div class="row">
+            <?php 
+                    if (  $querySearch->have_posts() ) : while (  $querySearch->have_posts() ) :  $querySearch->the_post(); ?>
+            <div class="col-md-4 col-xs-12 my-3">
+                <div class="fundo-branco-post">
+                    <a href="<?php the_permalink();?>"><img src="<?= the_post_thumbnail_url('medium-large');?>" alt="<?php the_title();?>" class="card-img-top"></a>
+                    <h4 class="pt-1 link-target">
+                        <a class="anchor-post" href="<?php the_permalink();?>">
+                            <?php the_title();?>
+                        </a>
+                    </h4>
+                </div>
+            </div>
+            <?php endwhile; 
+                    else : esc_html_e( 'Sorry, no posts matched your criteria.' ); endif; wp_reset_postdata();
+                ?>
+        </div>
+    </div>
+</div>
 <?php get_footer();?>
--- functions.php
+++ functions.php
@@ -15,13 +15,12 @@ remove_action('admin_print_styles', 'print_emoji_styles');
 //habilitar menu
 add_theme_support('menu');
 //registrar menu
-
 function register_my_menu()
 {
     register_nav_menu('menu-principal', __('Header Menu'));
 }
 add_action('init', 'register_my_menu');
-
+// remove classes dos links do menu
 function remove_css_id_filter($var)
 {
     return is_array($var) ? array_intersect($var, array('current-menu-item')) : '';
@@ -30,8 +29,12 @@ add_filter('page_css_class', 'remove_css_id_filter', 100, 1);
 add_filter('nav_menu_item_id', 'remove_css_id_filter', 100, 1);
 add_filter('nav_menu_css_class', 'remove_css_id_filter', 100, 1);
 
+// adiciona classe especifica para o link
 function add_specific_menu_location_atts( $atts, $item, $args ) {
     $atts['class'] = 'nav-link';
     return $atts;
 }
 add_filter( 'nav_menu_link_attributes', 'add_specific_menu_location_atts', 10, 3 );
+
+// adiciona suporte para thumbnails
+add_theme_support( 'post-thumbnails' );
